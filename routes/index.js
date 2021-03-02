00c69abe7d5465da41f4373f9f7b210e4c4cf51b
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-02 16:27:09 +0100

    update server routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
 
---
+++ routes/user.routes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const UserModel = require('../models/User.model.js')
+
+router.get('/:userId', (req, res) => {
+  UserModel.findById(req.params.userId)
+   .then((response) => {
+        res.status(200).json(response)
+   })
+   .catch((err) => {
+        res.status(500).json({
+             error: 'Something went wrong',
+             message: err
+        })
+   }) 
+})
+
+module.exports = router;
