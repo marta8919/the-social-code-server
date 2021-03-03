7f252cabb7f9221112ca00c7b2ecf567f689de3f
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-03 11:12:26 +0100

    update auth routes

--- app.js
+++ app.js
@@ -14,10 +14,42 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
+
+//---------------------------------------------------------
+// --------YOUR session config has been done here---------
+//---------------------------------------------------------
+// Set up connect-mongo
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default;
+
+app.use(session({
+    secret: 'NotMyAge',
+    saveUninitialized: false, 
+    resave: false, 
+    cookie: {
+      maxAge: 1000*60*60*24// is in milliseconds.  expiring in 1 day
+    },
+    store: new MongoStore({
+      mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/ReactTodos",
+      ttl: 60*60*24, // is in seconds. expiring in 1 day
+    })
+}));
+
+//---------------------------------------------------------
+//---------------------------------------------------------
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
-const allRoutes = require('./routes');
-app.use('/api', allRoutes);
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+const authRoutes = require('./routes/auth.routes');
+app.use('/', authRoutes);
+
+const postRoutes = require('./routes/post.routes');
+app.use('/', postRoutes);
+
+const userRoutes = require('./routes/user.routes');
+app.use('/', userRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
