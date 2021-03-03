7e1445f20eb2167e9a93e07fe01c9eba46b8012d
Marta <marta.gilabert.guzman@gmail.com>
2021-03-03 11:40:58 +0100

    handling error messages

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -67,14 +67,14 @@ router.post('/signin', (req, res) => {
   
   if ( !email || !password) {
       res.status(500).json({
-          error: 'Please enter Username. email and password',
+          errorMessage: 'Please enter Username. email and password',
      })
     return;  
   }
   const myRegex = new RegExp(/^[a-z0-9](?!.*?[^\na-z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-z0-9]$/);
   if (!myRegex.test(email)) {
       res.status(500).json({
-          error: 'Email format not correct',
+          errorMessage: 'Email format not correct',
       })
       return;  
   }
@@ -96,22 +96,22 @@ router.post('/signin', (req, res) => {
               //if passwords do not match
               else {
                   res.status(500).json({
-                      error: 'Passwords don\'t match',
+                      errorMessage: 'Passwords don\'t match',
                   })
                 return; 
               }
           })
           .catch(() => {
               res.status(500).json({
-                  error: 'Email format not correct',
+                  errorMessage: 'Email format not correct',
               })
             return; 
           });
     })
-    //throw an error if the user does not exists 
+    //throw an errorMessage if the user does not exists 
     .catch((err) => {
       res.status(500).json({
-          error: 'Email does not exist',
+          errorMessage: 'Email does not exist',
           message: err
       })
       return;  
